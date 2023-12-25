874500170f2e2110a91db32da199b805d58aa6f4
brantmerrell <brant.merrell@gmail.com>
2023-12-25 15:42:12 -0600

    submit fen

--- rb_ascii_chess/app.rb
+++ rb_ascii_chess/app.rb
@@ -2,42 +2,45 @@ require 'pgn'
 require 'sinatra'
 require 'stringio'
 
-startFen = PGN::FEN.start.to_s
 
 enable :sessions
 
 get '/' do
+  puts 'get /'
+  session[:fenState] ||= PGN::FEN.start.to_s
   session[:moves] ||= []
-  session[:current_position] ||= 0
+  session[:ndx_ply] ||= 0
+  session[:current_board] ||= PGN::FEN.new(session[:fenState]).to_position.board.inspect
 
-  if session[:moves].empty?
-    fen = PGN::FEN.new(startFen)
-    session[:current_board] = fen.to_position.board.inspect
-  end
   erb :index
 end
 
+post '/submit_fen' do
+  puts 'post /submit_fen'
+  session[:fenState] = params['fen'].strip
+  session[:moves] = []
+  session[:ndx_ply] = 0
+  session[:current_board] = PGN::FEN.new(session[:fenState]).to_position.board.inspect
+  redirect to('/')
+end
 
 post '/submit_move' do
-  puts 'submit_move'
+  puts 'post /submit_move'
   move = params['move'].strip
-  puts "move: #{move}"
   session[:moves] << move unless move.empty?
-  puts "moves: #{session[:moves].inspect}"
   game = PGN::Game.new(session[:moves])
-  puts "game: #{game.inspect}"
-  puts "positions: #{game.positions.inspect}"
-  session[:current_position] = game.positions.length - 1
-  session[:current_board] = game.positions[session[:current_position]].board.inspect
+  session[:ndx_ply] = game.positions.length - 1
+  session[:current_board] = game.positions[session[:ndx_ply]].board.inspect
   redirect to('/')
 end
 
 post '/navigate' do
-  session[:current_position] += params['direction'].to_i
-  session[:current_position] = 0 if session[:current_position] < 0
-  session[:current_position] = session[:moves].length if session[:current_position] > session[:moves].length
+  puts 'post /navigate'
+  session[:ndx_ply] += params['direction'].to_i
+  session[:ndx_ply] = 0 if session[:ndx_ply] < 0
+  session[:ndx_ply] = session[:moves].length if session[:ndx_ply] > session[:moves].length
   game = PGN::Game.new(session[:moves])
-  session[:current_board] = game.positions[session[:current_position]].board.inspect
+  session[:current_board] = game.positions[session[:ndx_ply]].board.inspect
   redirect to('/')
 end
 
