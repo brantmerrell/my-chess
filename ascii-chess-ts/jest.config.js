0cbd7f76df809e221bb27a2b7b525ba381bd2680
brantmerrell <brant.merrell@gmail.com>
2024-11-06 17:09:14 -0600

    cleanup files

---
+++ ascii-chess-ts/jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^axios$': require.resolve('axios'),
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(axios)/)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.json'
+    }],
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  setupFilesAfterEnv: [
+    '<rootDir>/src/setupTests.ts'
+  ],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json'
+    }
+  }
+};
