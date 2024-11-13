8a8aa7ae53245e3f57b499ad6a6902b1c08f05ba
brantmerrell <brant.merrell@gmail.com>
2024-11-13 11:02:43 -0600

    Remove legacy implementations now stored in legacy-frameworks branch

--- connector_rb/config/initializers/assets.rb
+++
@@ -1,4 +0,0 @@
-
-Rails.application.config.assets.version = "1.0"
-
-
--- connector_rb/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  get '/links', to: 'chess#links', format: false
-  get '/adjacencies', to: 'chess#adjacencies', format: false
-  put '/graphdag', to: 'chess#graphdag', format: false
-  get '/*path/', to: redirect { |params, req| req.path.chomp('/') }, format: false
-end
-
-
