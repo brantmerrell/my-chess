e24e20449a992b3fbc1d977f67df271dd89c7bb5
Joshua Merrell <josh.b.merrell@gmail.com>
2025-09-15 08:38:24 -0500

    Dockerfile OS-agnosticism

--- connector/getGraphdag.sh
+++ connector/getGraphdag.sh
@@ -1,2 +1,27 @@
-curl "http://127.0.0.1:8000/graphdag/?fen_string=rnbqkbnr%2Fpppppppp%2F8%2F8%2F8%2F8%2FPPPPPPPP%2FRNBQKBNR%20w%20KQkq%20-%200%201" 
+#!/bin/bash
+
+# Test graphdag endpoint by first getting links, then sending to graphdag
+
+echo "Step 1: Getting links from chess position..."
+LINKS_RESPONSE=$(curl -s "https://ascii-chess-connector-3fac027ebe4d.herokuapp.com/links/?fen_string=rnbqkbnr%2Fpppppppp%2F8%2F8%2F8%2F8%2FPPPPPPPP%2FRNBQKBNR%20w%20KQkq%20-%200%201")
+
+echo "Step 2: Extracting edges from links response..."
+# Extract just the edges array from the response
+EDGES=$(echo "$LINKS_RESPONSE" | python3 -c "
+import json
+import sys
+data = json.load(sys.stdin)
+edges_data = data.get('edges', [])
+# Convert to format expected by graphdag endpoint
+edges_formatted = [{'source': edge['source'], 'target': edge['target']} for edge in edges_data]
+print(json.dumps({'edges': edges_formatted}))
+")
+
+echo "Step 3: Sending edges to graphdag endpoint..."
+echo "$EDGES" | curl -X PUT "https://ascii-chess-connector-3fac027ebe4d.herokuapp.com/graphdag" \
+  -H "Content-Type: application/json" \
+  -d @-
+
+echo ""
+echo "Done!"
 
