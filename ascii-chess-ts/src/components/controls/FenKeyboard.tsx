e84b51f69ea413c2a01cc9cc79421d80b2fdd57d
joshua.merrell <joshua.merrell@inventive.io>
2026-01-17 07:30:52 -0600

    rearrange keyboard characters

--- ascii-chess-ts/src/components/controls/FenKeyboard.tsx
+++ ascii-chess-ts/src/components/controls/FenKeyboard.tsx
@@ -31,8 +31,9 @@ const FenKeyboard: React.FC<FenKeyboardProps> = ({
   const rows = [
     ["r", "n", "b", "q", "k", "p", "w"],
     ["R", "N", "B", "Q", "K", "P"],
-    ["0", "1", "2", "3", "4", "5", "6", "7", "8"],
-    ["/", "-", " "],
+    ["1", "2", "3", "4"],
+    ["5", "6", "7", "8"],
+    ["0", "/", "-", " "],
   ];
 
   const handleKeyClick = (key: string) => {
